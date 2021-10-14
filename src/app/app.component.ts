43c727cfe995eb2a29d9169a09748df7c03991bb
Deluxe <giobibiluri.gb@gmail.com>
2021-10-14 22:31:05 +0400

    created simple to do with drag n drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, CdkDrag, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'complex-to-do';
+  todo = [
+    'Write code',
+    'Do some stuff',
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep',
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ]
+
+  dropList: string[][] = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ]
+
+  activeList: number | null = null;
+  
+  drop(event: CdkDragDrop<string[]> |  CdkDragDrop<string[][]>) {
+    if(
+      !(Array.isArray(event.container.data[0]))
+    )
+    if (event.previousContainer === event.container) {
+      moveItemInArray((event.container.data as string[]), event.previousIndex, event.currentIndex);
+    } else {
+      console.log(event);
+      transferArrayItem((event.previousContainer.data as string[]),
+                        (event.container.data as string[]),
+                        event.previousIndex,
+                        event.currentIndex);
+    }else {
+      const prev =  this.dropList[event.previousIndex];
+      this.dropList[event.previousIndex] = this.dropList[event.currentIndex];
+      this.dropList[event.currentIndex] = prev;
+
+      const prevDay = this.weekdays[event.previousIndex];
+      this.weekdays[event.previousIndex] = this.weekdays[event.currentIndex];
+      this.weekdays[event.currentIndex] = prevDay;
+    }
+  }
 }
